58dc4561df8b9c5aa2ae907c87eded6f521252a8
Vova SKR <vova.skri.dev@gmail.com>
2020-01-05 14:49:20 +0300

    Init

---
+++ MISiS/MISiS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MISiS
+//
+//  Created by Vova SKR on 05.01.2020.
+//  Copyright © 2020 Vova Skripnickenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
